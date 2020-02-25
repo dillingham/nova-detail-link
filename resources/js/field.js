c069c9f52c4cf09a0deefe173ab7b7d9bc46f337
daanadriaan <daan@daanadriaan.nl>
2020-02-25 10:04:35 +0100

    Update field.js

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,3 +1,4 @@
 Nova.booting((Vue, router, store) => {
     Vue.component('index-nova-detail-link', require('./components/IndexField'))
+    Vue.component('index-nova-edit-link', require('./components/IndexEditField'))
 })
